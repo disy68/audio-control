34b1d17703f87f05defb8cd273bfda68212c6f5f
disy68 <szabolcs@diskay.hu>
2017-07-02 01:54:01 +0200

    Added fancy card layout, improved reload logic.

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,10 +1,46 @@
 (function(){
+  function startInterVal(isFirst) {
+    return setInterval(function(){ getWhitelistedDevices(); }, 4000);
+  }
+
+  var interval = startInterVal(true);
+
+  function clearMainInterval() {
+      clearInterval(interval);
+  }
+
+  function restartMainInterval() {
+    interval = startInterVal(false);
+  }
+
   var urlMapping = {
     whitelistedDevices : "/enabled-device",
     setVolume : "/set-volume"
   }
 
   var token = "{}";
+
+  function getCard(item) {
+    return '<div class="row">' +
+        '<div class="col s12 m6">' +
+          '<div class="card">' +
+            '<div class="card-content">' +
+              '<span class="card-title name">'
+                + item['deviceName'] +
+              '</span>' +
+              '<span class="card-value value">'
+                + item['volume'] +
+              '</span>' +
+              '<p class="range">' + token + '</p>' +
+            '</div>' +
+            '<div class="card-action">' +
+              '<a href="#">Mute</a>' +
+              '<a href="#">Unmute</a>' +
+            '</div>' +
+          '</div>' +
+        '</div>' +
+      '</div>';
+  }
 
   function getWhitelistedDevices() {
     var volumeRange = '<p class="range-field">' +
@@ -17,32 +53,25 @@
     $.getJSON(urlMapping.whitelistedDevices, function(data) {
       $whitelistDiv = $('#whitelist');
       $whitelistDiv.html("");
-      $ul = $("<ul></ul>");
 
       data.forEach(function(item) {
-        $range = $(volumeRange.replace(token, item['volume']));
-        $range.attr('id', item['deviceName']);
-        $li = $("<li><h4>" +
-                    " <span class='name'>"
-                      + item['deviceName'] +
-                    " </span> " +
-                    " <span class='value'>"
-                      + item['volume'] +
-                    "</span>" +
-                "</h4></li>");
-        $li.append($range);
-        $ul.append($li);
+        var range = volumeRange.replace(token, item['volume']);
+        var card =  getCard(item);
+        card = card.replace(token, range);
+        var $card = $(card);
+        $card.attr('id', item['deviceName']);
+        $whitelistDiv.append($card);
       });
-
-      $whitelistDiv.append($ul);
     })
   }
 
   function changeVolume(deviceName, volumeLevel) {
     var url = urlMapping.setVolume + "/" + deviceName + "/" + volumeLevel;
+    clearMainInterval();
 
     $.get(url, function(data) {
-      $('#' + deviceName).prev().children('span.value').first().text(volumeLevel);
+      $('#' + deviceName).find('span.value').first().text(volumeLevel);
+      restartMainInterval();
     });
   }
 
@@ -52,24 +81,14 @@
     $target = $(event.target);
 
     volume = $target.val();
-    device = $target.parent().attr('id');
+    device = $target.parent().siblings('span.name').first().text();
+
+    console.log(device);
 
     changeVolume(device, volume);
   });
 
-  function startInterVal(isFirst) {
-    return setInterval(function(){ getWhitelistedDevices(); }, 4000);
-  }
-
   // startup with a fancy delay ;-) and loading all the way :)
   setTimeout(function(){ getWhitelistedDevices(); }, 1500);
-  var interval = startInterVal(true);
 
-  function clearMainInterval() {
-      clearInterval(interval);
-  }
-
-  function restartMainInterval() {
-    interval = startInterVal(false);
-  }
 })();
